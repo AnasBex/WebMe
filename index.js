26124b5c84ffedad2d998fd2e3f95893f02d152d
Anas Bex <anasbex647@gmail.com>
2022-07-21 21:13:37 +0700

    JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('b1');
+  
+  link.addEventListener('click', function() {
+    
+    
+   var url = document.getElementById('main');
+   url.addEventListener('click'), function() {
+  
+  func();
+  
+   }
+   func();
+  })
+})
+
+var dl1 = "Sekarang Khusus Android aja gan:v";
+document.getElementById('dl');
