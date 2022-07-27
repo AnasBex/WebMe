1511b20a2bbe407dd23ed16c5172e9d0c7a197b1
anasbex647@gmail.com <anasbex647@gmail.com>
2022-07-27 20:30:32 +0700

    New

--- keyAds/key.js
+++ keyAds/key.js
@@ -1,3 +1,5 @@
 var ads1 = 'https://www.highperformancegate.com/xdpeun54?key=1e229c4d3de9de21996ac54692270b60';
 
 var file = 'https://drive.google.com/file/d/1n0_S2ta0Cuex5xSdWz31lX8AizAOQ_rW/view?usp=drivesdk'
+
+var url="printTables.php?option=6";
